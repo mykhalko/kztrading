532bb62541b225cf4d5e4956180231bd1ae412b4
Vladyslav Mykhalko <mykhalko.edu@gmail.com>
2018-10-07 14:54:52 +0300

    Modify Confirmation model
     - add field expiration (time when link will become inactive)
     - add field visited (tells if link was visited)

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,6 @@
+import datetime
+import uuid
+
 from django.contrib.auth.models import (
     AbstractBaseUser,
     PermissionsMixin,
@@ -51,3 +54,14 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return str(self.email)
+
+
+class Confirmation(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid1, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    expiration = models.DateTimeField(default=lambda: timezone.now() + datetime.timedelta(days=1))
+    visited = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.user) + ' ' +  str(self.id)
