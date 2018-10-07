fcf97f0329a3d1d15c824397853acc45a743d785
Vladyslav Mykhalko <mykhalko.edu@gmail.com>
2018-10-07 09:59:10 +0300

    Add user model & user manager

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,49 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager,
+)
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password, name, surname, phone_number,
+                    is_active=None, is_staff=None, is_admin=None):
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.name = name
+        user.surname = surname
+        user.phone_number = phone_number
+        user.is_active = is_active
+        user.is_admin = is_admin
+        user.is_staff = is_staff
+        user.save(using=self._db)
+        return user
+
+    def create_staffuser(self, email, password, name=None, surname=None, phone_number=None):
+        self.create_user(email, password, name, surname, is_active=True, is_staff=True)
+
+    def create_superuser(self, email, password, name=None, surname=None, phone_number=None):
+        self.create_user(email, password, name, surname, phone_number,
+                         is_active=True, is_staff=True, is_admin=True)
+
+
+class User(AbstractBaseUser):
+
+    email = models.EmailField(max_length=255, unique=True, empty=False, blank=False)
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=32)
+    registration_date = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(default=False, verbose_name='active')
+    is_staff = models.BooleanField(default=False, verbose_name='staff')
+    is_admin = models.BooleanField(default=False, verbose_name='admin')
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return str(self.email)
