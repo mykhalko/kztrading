1a28b4ba82a20da973443630e375d656b599e1b4
Vladyslav Mykhalko <mykhalko.edu@gmail.com>
2018-10-07 14:23:25 +0300

    Add initial views for Login/Register

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.views.generic import FormView
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from . import forms
+
+
+class LoginView(FormView):
+    template_name = 'accounts/default.html'
+    form_class = forms.LoginForm
+
+    def get_form(self, form_view = None):
+        return self.form_class(request=self.request, **self.get_form_kwargs())
+
+    def get_success_url(self):
+        return 'accounts/login_success'
+
+    def form_valid(self, form):
+        print('# form valid')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        print('# form invalid')
+        return super().form_invalid(form)
+
+
+class RegisterView(FormView):
+    template_name = 'accounts/default.html'
+    form_class = forms.RegistrationForm
+
+    def get_success_url(self):
+        return 'accounts/register_success'
+
+    def form_valid(self, form):
+        print('# form valid')
+        form.save()
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        print('# form invalid')
+        return  super().form_invalid(form)
