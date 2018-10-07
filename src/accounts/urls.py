dacc8974b73c2048ab44a7b7034536aef3c0acf4
Vladyslav Mykhalko <mykhalko.edu@gmail.com>
2018-10-07 18:10:52 +0300

    Configure accounts urls

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url('login/$', views.LoginView.as_view()),
     url('register/$', views.RegisterView.as_view()),
+    url('confirm/(?P<uuid_key>[0-9a-zA-Z-]+)/$', views.ConfirmView.as_view())
 ]
