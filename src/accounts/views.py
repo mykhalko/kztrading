bda37feff0a0c7d703b50a1604452c42af70aa12
Vladyslav Mykhalko <mykhalko.edu@gmail.com>
2018-10-07 18:00:15 +0300

    Add new view, modify previous for accounts
    Login view:
     Now log user in when form is valid.
     Remove print statements from form valid/invalid
    
    Register view:
     Now when form is valid create confirmation record
     & send message with confirmation link to user email
     Remove print statement form form valid/invalid
    
    Confirm View:
     If visited, change user status to active, and confirmation to visited
    
    registration success view:
     redirect user here after registration

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,7 +1,12 @@
-from django.views.generic import FormView
+from django.core.mail import EmailMessage
+from django.core.exceptions import ValidationError
+from django.contrib.auth import login
+from django.views.generic import FormView, DetailView
 from django.shortcuts import render, redirect
+from django.http import Http404
 
 from . import forms
+from .models import Confirmation
 
 
 class LoginView(FormView):
@@ -15,11 +20,10 @@ class LoginView(FormView):
         return 'accounts/login_success'
 
     def form_valid(self, form):
-        print('# form valid')
-        return super().form_valid(form)
+        login(request=self.request, user=form.user)
+        return redirect('/')
 
     def form_invalid(self, form):
-        print('# form invalid')
         return super().form_invalid(form)
 
 
@@ -31,10 +35,63 @@ class RegisterView(FormView):
         return 'accounts/register_success'
 
     def form_valid(self, form):
-        print('# form valid')
         form.save()
-        return super().form_valid(form)
+        user = form.instance
+        confirmation = Confirmation.objects.create_confirmation_link(user)
+        self.send_confirmation(confirmation)
+        return redirect('/accounts/registration_success/')
 
     def form_invalid(self, form):
-        print('# form invalid')
-        return  super().form_invalid(form)
+        return super().form_invalid(form)
+
+    def send_confirmation(self, confirmation):
+        request = self.request
+        subject = 'Please verify your email address'
+        link = ''.join(['http://', request.get_host(), '/accounts/confirm/', str(confirmation.id)])
+        message_raw = \
+            'Hi! Thank you for getting started with kztrading.\n' \
+            'We need a little bit more information to complete your registration.' \
+            'Click link below to confirm your email address.\n\n{}\n\n' \
+            'Didn’t request this email?\nNo worries! Your address may have been ' \
+            'entered by mistake. If you ignore or delete this email, nothing further ' \
+            'will happen.'
+        message = message_raw.format(link)
+        mail = EmailMessage(subject, message, to=[confirmation.user.email])
+        mail.send()
+
+
+def registration_success_view(request):
+    return render(request, 'accounts/registration_success.html', {})
+
+
+class ConfirmView(DetailView):
+    template_name = 'accounts/confirm.html'
+    model = Confirmation
+
+    object = None
+    slug_field = 'id'
+    slug_url_kwarg = 'uuid_key'
+
+    def get_object(self, queryset=None):
+        try:
+            obj = self.model.objects.get(id=self.kwargs.get(self.slug_url_kwarg))
+            self.object = obj
+        except self.model.DoesNotExist as ex:
+            raise Http404()
+        except ValidationError as ex:
+            raise Http404()
+        return obj
+
+    def get_context_data(self, **kwargs):
+        email = self.object.user.email
+        return {'email': email}
+
+    def get(self, requset, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.visited = True
+        self.object.user.is_active = True
+        self.object.user.save()
+        self.object.save()
+        context = self.get_context_data()
+        return self.render_to_response(context)
+
