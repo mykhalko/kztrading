1e505d8bdb58583573f45ad0713db394e3055a78
Vladyslav Mykhalko <mykhalko.edu@gmail.com>
2018-10-08 09:24:30 +0300

    Add Confirmation Model to admin panel

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 
 from django.contrib.auth import get_user_model
 
+from .models import Confirmation
+
 
 User = get_user_model()
 
 admin.site.register(User)
+admin.site.register(Confirmation)
