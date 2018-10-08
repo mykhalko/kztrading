95c743af1b4abfde215d3b7188da928ad9cb1d63
Vladyslav Mykhalko <mykhalko.edu@gmail.com>
2018-10-08 11:16:19 +0300

    Register prodcuts to admin panel & account migrations

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Image, Category
+
+
+admin.site.register(Product)
+admin.site.register(Image)
+admin.site.register(Category)
