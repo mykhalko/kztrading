3bdfe052f165aa9e6912af4d7a64a100760bf83c
Vladyslav Mykhalko <mykhalko.edu@gmail.com>
2018-10-08 09:24:03 +0300

    Configured accounts.urls & set name for links

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -2,8 +2,14 @@ from django.conf.urls import url
 
 from . import views
 
+
+app_name = 'accounts'
+
+
 urlpatterns = [
-    url('login/$', views.LoginView.as_view()),
-    url('register/$', views.RegisterView.as_view()),
-    url('confirm/(?P<uuid_key>[0-9a-zA-Z-]+)/$', views.ConfirmView.as_view())
+    url('login/$', views.LoginView.as_view(), name='login'),
+    url('register/$', views.RegisterView.as_view(), name='register'),
+    url('confirm/(?P<uuid_key>[0-9a-zA-Z-]+)/$', views.ConfirmView.as_view()),
+    # url('registration_success/$', views.registration_success_view),
+    url('logout/$', views.logout_view, name='logout'),
 ]
