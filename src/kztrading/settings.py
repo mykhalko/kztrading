9e9907026cc23cf29f7dddaeb634d0bcae8abe6d
Vladyslav Mykhalko <mykhalko.edu@gmail.com>
2018-10-09 20:38:03 +0300

    Create orders app & configured & make migrations

--- src/kztrading/settings.py
+++ src/kztrading/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'products',
     'accounts',
     'cart',
+    'orders'
 ]
 
 MIDDLEWARE = [
--- src/orders/models.py
+++ src/orders/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from products.models import Product
+
+User = get_user_model()
+
+
+class Order(models.Model):
+
+    product = models.OneToOneField(Product, null=True, on_delete=models.SET_NULL)
+    buyer = models.OneToOneField(User, null=True, on_delete=models.SET_NULL)
+    datetime = models.DateTimeField(auto_created=True, editable=False)
