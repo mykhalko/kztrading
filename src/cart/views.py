f81e20cc8eea26b5c67bdf51445699682b77a9cc
Vladyslav Mykhalko <mykhalko.edu@gmail.com>
2018-10-09 19:26:04 +0300

    Add CartItemsView&template for cart app

--- src/cart/views.py
+++ src/cart/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from functools import reduce
 
-# Create your views here.
+from django.db.models import Q
+from django.shortcuts import render
+from django.views.generic import ListView
+
+from products.models import Product
+
+
+class CartItemsView(ListView):
+    template_name = 'cart/defaultcart.html'
+
+    def get_queryset(self):
+        items = self.request.get('cart_items')
+        if not items:
+            return None
+        query = [Q(id=item_id) for item_id in items]
+        queryset = Product.objects.filter(query)
+        return queryset
